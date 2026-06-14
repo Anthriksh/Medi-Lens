608f8e6a6d681525358c5d2cf98147186e4af617
Anthriksh <mohitheanthriksh@gmail.com>
2026-06-14 23:01:40 +0530

    Connect frontend to Render backend

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,14 @@ import {
 import {
   getFirestore,
 } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDVUlpJJAlxcoTRYKb5KrFtLl9M9LBWR5E",
-  authDomain: "medilens-b0277.firebaseapp.com",
-  projectId: "medilens-b0277",
-  storageBucket: "medilens-b0277.firebasestorage.app",
-  messagingSenderId: "400666420242",
-  appId: "1:400666420242:web:7e6c82c1525b975189a4cb",
-  measurementId: "G-SNMY4PWXX2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
